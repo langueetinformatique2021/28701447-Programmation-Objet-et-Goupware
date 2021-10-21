2e9c585b02a7055535a0c932b9df612614730b28
LaurentHPP <penghao123321@163.com>
2021-10-21 12:02:43 +0200

    peng

---
+++ TD4/src/td4_1_2.java
@@ -0,0 +1,18 @@
+
+public record td4_1_2() {
+
+}
+package atelier04;
+
+public class Conditionnelle {
+
+	public static void main(String[] args) {
+		int p1 = 10, p2 = 9, p3 = -1, p4 = 4, p5 = 8;
+		
+		
+		
+		System.out.println(p4);
+
+	}
+
+}
